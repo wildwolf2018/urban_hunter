fc93f75561dce28066d8e54a45e70a9564aa16b0
1886081 <1886081@students.wits.ac.za>
2018-07-10 16:06:28 +0200

    Revert "Added project files"
    This reverts commit 84d7ddcf52188aa8cb71ca05d41298058a99d1ae.

--- Assets/Scripts/Enemy/EnemyHealthBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class EnemyHealthBase : MonoBehaviour {
-	protected abstract void Awake();
-	protected abstract void Update();
-	public abstract void Damage(int amount);
-}
